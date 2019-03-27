b876274f8bbd950805b1d4d1fea71f7e9f301e30
rxp8764 <ramkumar_paranjothy@homedepot.com>
2019-03-27 11:29:24 -0500

    pushing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <div className="container info">
-      Hello I am looking for a job
-        </div>
+        THis is test
       </div>
     );
   }
